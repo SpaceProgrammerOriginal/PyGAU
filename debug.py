fc23441be24f557535137d6ce8e30999dd81f807
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2025-06-20 19:41:59 +0200

    Added debug tools

---
+++ debug.py
@@ -0,0 +1,17 @@
+class Report:
+
+    """
+    Report/trace/print each function as you call. As simply as adding a decorator to them.
+    """
+
+    def report(func):
+
+        def modified_behaviour(*args, **kwargs):
+
+            print("Started execution of", func.__name__)
+
+            bullshit = func(*args, **kwargs)
+
+            print("Ended execution of", func.__name__)
+
+        return modified_behaviour
