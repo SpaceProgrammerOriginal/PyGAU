7d5d956013d1fa20cea6f18fd66e57a1f1a3a366
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2025-06-21 13:34:11 +0200

    Added report_func + fixed bugs
    The old "report" decorator is now deprecated, will be deleted in version 1.2

--- debug.py
+++ debug.py
@@ -1,9 +1,12 @@
+from warnings import deprecated as _deprecated
+
 class Report:
 
     """
     Report/trace/print each function as you call. As simply as adding a decorator to them.
     """
 
+    @_deprecated("Use 'report_function' decorator instead. This decorator will get deleted in version 1.2")
     def report(func):
 
         """
@@ -18,4 +21,38 @@ class Report:
 
             print("Ended execution of", func.__name__)
 
+            return bullshit #fixed little bug
+
         return modified_behaviour
+    
+    def report_function(report_end: bool = True, enable: bool = True):
+
+        """
+        Decorator for reporting when a function starts executing and ends.
+
+        As parameters, it takes "report_end" that indicates whatever it should also report when the function stops execution.
+        It also takes "debug" that indicates if the report is done or not.
+        """
+
+        def decorator(func):
+
+            if enable:
+
+                def modified_behaviour(*args, **kwargs):
+
+                    print("Started execution of", func.__name__)
+
+                    bullshit = func(*args, **kwargs)
+
+                    if report_end:
+                        print("Ended execution of", func.__name__)
+
+                    return bullshit
+
+                return modified_behaviour
+            
+            else:
+
+                return func #do no changes
+        
+        return decorator
